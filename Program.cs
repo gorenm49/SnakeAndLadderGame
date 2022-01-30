6005888224f87fed1f3005e99db0305216c9e708
Nitya <gorenityanand96@gmail.com>
2022-01-30 21:33:00 +0530

    UC1 is done

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder game!!!");
+        }
+        
+    }
+}
