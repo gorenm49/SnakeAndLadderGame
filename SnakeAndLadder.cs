e3f523ef47893f21ec500a7bf89985561952fb8a
Nitya <gorenityanand96@gmail.com>
2022-01-30 21:40:20 +0530

    UC2 is done

--- SnakeAndLadder.cs
+++ SnakeAndLadder.cs
@@ -5,5 +5,13 @@ namespace SnakeAndLadder
     public class SnakeAndLadderGame
     {
         int playerPosition = 0; 
+
+        Random random = new Random();
+
+        public void DiceRoll()
+        {
+            int num = random.Next(1,7);
+            Console.WriteLine(num);
+        }
     }    
 }
